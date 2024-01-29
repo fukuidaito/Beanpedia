682b39f2b12293844b6fc5df5ef8a1bddf6d6fa4
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-01-30 08:58:14 +0900

    ログアウト機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ before_save { self.email = email.downcase }
   validates :email, presence: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX },uniqueness: true
 
   has_secure_password
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
