6adc2b678ad55b0de46161c6fd8ddb595702d956
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-01-31 09:01:58 +0900

    画像がヘッダーに表示されるように調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
+  mount_uploader :avatar, AvatarUploader
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
