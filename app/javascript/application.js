6307dd493fa8a91ecb2d04b29370cd7c5705830e
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-02-06 21:17:01 +0900

    不要なJS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,3 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require rails-ujs
-//= require activestorage
-//= require_tree .
-
-
 import "@hotwired/turbo-rails"
 import "controllers"
 import "custom/menu"
