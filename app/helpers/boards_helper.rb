e263d7e08e2b2cdee5ab4de2df92620b8ddfb1f3
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-02-13 08:07:34 +0900

    動的にコーヒーの特徴を表示させることに成功

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -1,2 +1,18 @@
 module BoardsHelper
+  def enum_to_percentage(attribute)
+  case attribute
+    when "very_low"
+        20
+    when "low"
+        40
+    when "medium"
+        60
+    when "high"
+        80
+    when "very_high"
+        100
+    else
+        0 # デフォルト値またはエラー値
+    end  
+  end
 end
