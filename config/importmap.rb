1b618523bcd104f141a15d0c89ba9640a89d36b3
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-02-20 08:48:09 +0900

    コンソールのエラーを訂正

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,6 +3,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 
 pin "custom/menu", to: "menu.js", preload: true
 pin "custom", to: "custom.js", preload: true
-
-# BootstrapをCDNからピン留めする場合の例（BootstrapとPopperを含むバンドル版）
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", preload: true
