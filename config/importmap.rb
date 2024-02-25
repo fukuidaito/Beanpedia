0d934413eb7b7407f3822a01e61a2678aa6839bc
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-02-26 08:16:12 +0900

    画像投稿前のプレビュー表示に成功

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "custom/menu", to: "menu.js", preload: true
 pin "custom", to: "custom.js", preload: true
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", preload: true
 pin "top", to: "top.js"
+pin "preview", to: "preview.js"
