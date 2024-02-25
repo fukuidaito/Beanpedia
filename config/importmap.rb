b74b696827f952f8300aed793d77094118e2800f
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-02-26 07:46:22 +0900

    importmapでjsファイルの読み込みを切り分け

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,3 +4,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "custom/menu", to: "menu.js", preload: true
 pin "custom", to: "custom.js", preload: true
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", preload: true
+pin "top", to: "top.js"
