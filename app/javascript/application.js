04de16dd6bf778d83dbeb086ab4fdda7a0495776
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-03-01 08:11:53 +0900

    検索ボタンの場所の調整

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,3 @@
-import "@hotwired/turbo-rails";
-
-// import { Turbo } from "@hotwired/turbo-rails"
-// Turbo.session.drive = false
+// import "@hotwired/turbo-rails";
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
