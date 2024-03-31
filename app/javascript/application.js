4e7458656f134b68dee94e0694f93633422242e7
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-04-01 08:52:32 +0900

    rails/ujsの導入

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,9 @@
 import "@hotwired/turbo-rails";
+
+import Rails from "@rails/ujs";
+
+Rails.start();
+
 import jquery from "jquery";
 
 window.$ = jquery;
-import Raty from "./raty";  // Assuming raty.js is in the same directory
-
-window.raty = function(elem,opt) {
-  let raty =  new Raty(elem,opt)
-  raty.init();
-  return raty;
-}
