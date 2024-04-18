05e2dd23278dd61480966bae1ea2a4c44de74309
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-04-19 08:54:33 +0900

    lineログイン機能実装成功

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,10 @@
-# frozen_string_literal: true
-
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
   def google_oauth2
     callback_for(:google)
+  end
+
+  def line
+    callback_for(:line)
   end
 
   def callback_for(provider)
@@ -16,10 +17,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     redirect_to root_path
   end
 
-  def line
-    basic_action
-  end
-
   private
   
   def basic_action
@@ -28,17 +25,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @profile = User.find_or_initialize_by(provider: @omniauth["provider"], uid: @omniauth["uid"])
       if @profile.email.blank?
         email = @omniauth["info"]["email"] ? @omniauth["info"]["email"] : "#{@omniauth["uid"]}-#{@omniauth["provider"]}@example.com"
-        @profile = current_user || User.create!(provider: @omniauth["provider"], uid: @omniauth["uid"], email: email, name: @omniauth["info"]["name"], password: Devise.friendly_token[0, 20])
+        @profile = current_user || User.create!(
+          provider: @omniauth["provider"],
+          uid: @omniauth["uid"],
+          email: email,
+          name: @omniauth["info"]["name"],
+          password: Devise.friendly_token[0, 20]
+        )
       end
       @profile.set_values(@omniauth)
       sign_in(:user, @profile)
     end
-    #ログイン後のflash messageとリダイレクト先を設定
+    # ログイン後のリダイレクト先を root_path に設定
     flash[:notice] = "ログインしました"
-    redirect_to expendable_items_path
-  end
-
-  def fake_email(uid, provider)
-    "#{auth.uid}-#{auth.provider}@example.com"
+    redirect_to root_path
   end
 end
