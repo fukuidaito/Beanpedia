562b685c971a853671513df55e04bf347d535a5d
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-04-27 23:52:02 +0900

    ログイン後のアクセスを変更

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,6 @@ pin "jquery" # @3.7.1
 pin "raty", to: "raty.js", preload: true
 pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
 pin "preview", to: "preview.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "stimulus-autocomplete"
+pin "stimulus-autocomplete", to: "https://ga.jspm.io/npm:stimulus-autocomplete@3.1.0/src/autocomplete.js" 
