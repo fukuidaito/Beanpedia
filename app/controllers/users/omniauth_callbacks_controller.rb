8b21da092f88b915a0610918a093d049d4e7499c
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-05-13 08:22:03 +0900

    rubocopのusers_comtrollerを修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,10 +9,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def callback_for(provider)
     @user = User.from_omniauth(request.env['omniauth.auth'])
-    
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:success, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+      set_flash_message(:success, :success, kind: provider.to_s.capitalize) if is_navigational_format?
       PushLineJob.perform_later(@user.id)
     else
       session["devise.#{provider}_data"] = request.env['omniauth.auth']
