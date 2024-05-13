2f1a70f84fb78966b2d6d3b29e9218c611b74aea
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-05-14 07:59:33 +0900

    rubocopの指摘を修正

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "StaticPages", type: :request do
-  describe "GET /terms_of_service" do
-    it "returns http success" do
-      get "/static_pages/terms_of_service"
+RSpec.describe 'StaticPages', type: :request do
+  describe 'GET /terms_of_service' do
+    it 'returns http success' do
+      get '/static_pages/terms_of_service'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
