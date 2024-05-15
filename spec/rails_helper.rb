c1a12571f4b448c4f3487624b5f42da061bc5fb6
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-05-16 08:23:52 +0900

    CI

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,6 +63,22 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
 
+
+require 'capybara/rspec'
+require 'selenium-webdriver'
+
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--window-size=1920,1080')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :selenium_chrome_headless
+
 config.before(:each, type: :system) do
   driven_by :selenium_chrome_headless
 end
