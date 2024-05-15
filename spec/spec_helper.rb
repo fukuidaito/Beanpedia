2b6bb04e5259210b1378dca817ca24274e46ba32
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-05-16 08:42:39 +0900

    a

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -94,12 +94,12 @@ RSpec.configure do |config|
   Capybara.register_driver :selenium_chrome_headless do |app|
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--headless')
-    options.add_argument('--disable-gpu') 
-    options.add_argument('--no-sandbox') 
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
     options.add_argument('--window-size=1920,1080')
-  
+
     Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
   end
-  
+
   Capybara.javascript_driver = :selenium_chrome_headless
 end
