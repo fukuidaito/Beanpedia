7958d50b6aac4cf237b1ccccd32d579ed6e201fe
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-05-20 08:25:20 +0900

    ログイン前にboardsに入ったときのログイン画面をしゅうせい

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,21 +2,27 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+    flash[:danger] = "ログインに失敗しました。メールアドレスまたはパスワードが間違っています。"
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  rescue
+    flash[:danger] = "ログインに失敗しました。メールアドレスまたはパスワードが間違っています。"
+    redirect_to new_session_path(resource_name)
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
   # protected
 
