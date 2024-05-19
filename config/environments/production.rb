4966a2828a463437bb2c68778d1966573e4bb373
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-05-19 12:48:54 +0900

    config.ssl_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,11 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  config.ssl_options = {
+    redirect: {
+      exclude: ->(request) { request.path =~ /health_check/ || request.x_forwarded_proto == 'https' }
+    }
+  }
 
 
   # Include generic and useful information about system operation, but avoid logging too much
