b05c074df75a4add1116a40210cbdbb6702b9661
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-05-23 21:04:33 +0900

    テストに今度こそパス

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -3,46 +3,46 @@ require 'rails_helper'
 RSpec.describe SessionsHelper, type: :helper do
   let(:user) { create(:user) }
 
-  describe "#log_in" do
-    it "logs in the user and sets the session" do
+  describe '#log_in' do
+    it 'logs in the user and sets the session' do
       log_in(user)
       expect(session[:user_id]).to eq(user.id)
       expect(session[:session_token]).to eq(user.session_token)
     end
   end
 
-  describe "#remember" do
-    it "remembers the user in persistent session" do
+  describe '#remember' do
+    it 'remembers the user in persistent session' do
       remember(user)
       expect(cookies.permanent.encrypted[:user_id]).to eq(user.id)
       expect(cookies.permanent[:remember_token]).to eq(user.remember_token)
     end
   end
 
-  describe "#logged_in?" do
-    context "when user is logged in" do
+  describe '#logged_in?' do
+    context 'when user is logged in' do
       before do
         allow(helper).to receive(:current_user).and_return(user)
       end
 
-      it "returns true" do
+      it 'returns true' do
         expect(helper.logged_in?).to be_truthy
       end
     end
 
-    context "when no user is logged in" do
+    context 'when no user is logged in' do
       before do
         allow(helper).to receive(:current_user).and_return(nil)
       end
 
-      it "returns false" do
+      it 'returns false' do
         expect(helper.logged_in?).to be_falsey
       end
     end
   end
 
-  describe "#forget" do
-    it "forgets the user" do
+  describe '#forget' do
+    it 'forgets the user' do
       remember(user)
       forget(user)
       expect(cookies[:user_id]).to be_nil
