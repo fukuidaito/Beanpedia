de54a60d3c794622b8617fd8e16d30fe0e0531d4
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-05-27 07:57:31 +0900

    assign_meta_tagsメソッドの分割

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   include Pagy::Frontend
+
   def show_meta_tags
     assign_meta_tags if display_meta_tags.blank?
     display_meta_tags
@@ -8,36 +9,52 @@ module ApplicationHelper
   def assign_meta_tags(options = {})
     defaults = t('meta_tags.defaults', default: {}).with_indifferent_access
     options.reverse_merge!(defaults)
+
+    configs = build_meta_tags_config(options)
+    set_meta_tags(configs)
+  end
+
+  private
+
+  def build_meta_tags_config(options)
     site = options[:site]
     title = options[:title]
     description = options[:description]
     keywords = options[:keywords]
     image = options[:image].presence || image_url('coffetop.png')
-    configs = {
+
+    {
       separator: '|',
       reverse: true,
-      site: site,
-      title: title,
-      description: description,
-      keywords: keywords,
+      site:,
+      title:,
+      description:,
+      keywords:,
       canonical: request.original_url,
       icon: {
         href: image_url('coffetop.png')
       },
-      og: {
-        type: 'website',
-        title: title.presence || site,
-        description: description,
-        url: request.original_url,
-        image: image,
-        site_name: site
-      },
-      twitter: {
-        site: site,
-        card: 'summary_large_image',
-        image: image
-      }
+      og: build_og_tags(site, title, description, image),
+      twitter: build_twitter_tags(site, image)
     }
-    set_meta_tags(configs)
+  end
+
+  def build_og_tags(site, title, description, image)
+    {
+      type: 'website',
+      title: title.presence || site,
+      description:,
+      url: request.original_url,
+      image:,
+      site_name: site
+    }
+  end
+
+  def build_twitter_tags(site, image)
+    {
+      site:,
+      card: 'summary_large_image',
+      image:
+    }
   end
 end
