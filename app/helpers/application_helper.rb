fc3d4879a0fe973e91d87bac50c5bd8e3aef9630
rq-fukuidaito-at-321899771753 <bbawaz12@gmail.com>
2024-06-29 20:46:32 +0900

    ogp設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,60 +1,47 @@
 module ApplicationHelper
   include Pagy::Frontend
 
-  def show_meta_tags
-    assign_meta_tags if display_meta_tags.blank?
-    display_meta_tags
+  def default_meta_tags
+    {
+      site: 'beanpedia',
+      title: 'コーヒーシェアリングサイト',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'お気に入りのコーヒーを投稿して広めよう！',
+      keywords: 'コーヒー,coffee',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('sumaho.png'),
+        local: 'ja-JP'
+      },
+
+      twitter: {
+        card: 'summary_large_image',
+        site: '@zutozako1',
+        image: image_url('sumaho.png')
+      }
+    }
   end
 
   def assign_meta_tags(options = {})
-    defaults = t('meta_tags.defaults', default: {}).with_indifferent_access
-    options.reverse_merge!(defaults)
+  defaults = t('meta_tags.defaults', default: {}).with_indifferent_access
+  options.reverse_merge!(defaults)
 
-    configs = build_meta_tags_config(options)
-    set_meta_tags(configs)
+  if controller_name == 'boards' && action_name == 'show'
+    options[:image] = @board.image_url if @board.present? && @board.image_url.present?
+    options[:title] = @board.title if @board.present? && @board.title.present?
+    options[:description] = @board.body if @board.present? && @board.body.present?
   end
 
-  private
+  Rails.logger.debug "Meta tags options: #{options.inspect}"
 
-  def build_meta_tags_config(options)
-    site = options[:site]
-    title = options[:title]
-    description = options[:description]
-    keywords = options[:keywords]
-    image = options[:image].presence || image_url('coffetop.png')
-
-    {
-      separator: '|',
-      reverse: true,
-      site:,
-      title:,
-      description:,
-      keywords:,
-      canonical: request.original_url,
-      icon: {
-        href: image_url('coffetop.png')
-      },
-      og: build_og_tags(site, title, description, image),
-      twitter: build_twitter_tags(site, image)
-    }
-  end
-
-  def build_og_tags(site, title, description, image)
-    {
-      type: 'website',
-      title: title.presence || site,
-      description:,
-      url: request.original_url,
-      image:,
-      site_name: site
-    }
-  end
-
-  def build_twitter_tags(site, image)
-    {
-      site:,
-      card: 'summary_large_image',
-      image:
-    }
-  end
+  configs = build_meta_tags_config(options)
+  set_meta_tags(configs)
+end
 end
